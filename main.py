7fd0a0f8687339e95479885f7169356db889d0bb
Hoon Yim <yimsh4507@gmail.com>
2022-02-15 02:52:09 -0500

    Make purchase

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Ms. Laily Ajeliu
+from genericpath import exists
 import sys
 from wcwidth import wcswidth
 from input_validation import *
@@ -7,6 +8,8 @@ employee_list = []
 item_list = []
 
 # Asks if the use wants to store more employees or items
+
+
 def close_or_not(type):
     while True:
         yes_or_no = input(f"Another {type}? [y/n] ")
@@ -25,6 +28,8 @@ def close_or_not(type):
             print("Enter between 'y' or 'n'..")
 
 # Checks if the employee id or discount is unique
+
+
 def is_employee_element_unique(value, index):
     for employee in employee_list:
         if employee[index] == value:
@@ -32,6 +37,8 @@ def is_employee_element_unique(value, index):
     return True
 
 # Creating Employee
+
+
 def create_employee():
     NO_entered = False
     halt_statement = "Creating Employee Halt"
@@ -40,7 +47,8 @@ def create_employee():
     if NO_entered:
         print(halt_statement)
         return
-    employee_name, NO_entered = str_input("Please enter the name of Employee: ")
+    employee_name, NO_entered = str_input(
+        "Please enter the name of Employee: ")
     if NO_entered:
         print(halt_statement)
         return
@@ -48,13 +56,15 @@ def create_employee():
     if NO_entered:
         print(halt_statement)
         return
-    worked_years, NO_entered = int_input("Please enter how many years this Employee worked: ")
+    worked_years, NO_entered = int_input(
+        "Please enter how many years this Employee worked: ")
     if NO_entered:
         print(halt_statement)
         return
     total_purchased = 0
     total_discount = 0
-    employee_discount, NO_entered = int_input("Please enter the Employee's discount number: ")
+    employee_discount, NO_entered = int_input(
+        "Please enter the Employee's discount number: ")
     if NO_entered:
         print(halt_statement)
         return
@@ -64,12 +74,15 @@ def create_employee():
         print(f"Employee ID {employee_id} exists in the list already..")
         return
     if not is_employee_element_unique(employee_discount, 6):
-        print(f"Employee Discount {employee_discount} exists in the list already..")
+        print(
+            f"Employee Discount {employee_discount} exists in the list already..")
         return
 
-    temp_list = [employee_id, employee_name, employee_type, worked_years, total_purchased, total_discount, employee_discount]
+    temp_list = [employee_id, employee_name, employee_type,
+                 worked_years, total_purchased, total_discount, employee_discount]
     employee_list.append(temp_list)
     print(employee_list)
+
 
 def create_employee_page():
     while True:
@@ -77,10 +90,12 @@ def create_employee_page():
         while True:
             if close_or_not("Employee"):
                 return
-            else: 
+            else:
                 break
 
 # Checks if the item id is unique
+
+
 def is_item_id_unique(id):
     for item in item_list:
         if item[0] == id:
@@ -88,6 +103,8 @@ def is_item_id_unique(id):
     return True
 
 # Creating Item
+
+
 def create_item():
     NO_entered = False
     halt_statement = "Creating Employee Halt"
@@ -114,6 +131,7 @@ def create_item():
     item_list.append(temp_list)
     print(item_list)
 
+
 def create_item_page():
     while True:
         create_item()
@@ -125,51 +143,90 @@ def create_item_page():
 
 # Making Purchase
 # for formatting
+
+
 def wcpadding(s, l):
     return s + " " * (l - wcswidth(s))
+
 
 def print_items():
     if len(item_list) == 0:
         print("There is no Item..")
         return False
     else:
-        print("{0}|{1}|{2}".format(wcpadding("Item Number", 12), wcpadding("Item Name", 12), wcpadding("Item Cost", 12)))
+        print("{0}|{1}|{2}".format(wcpadding("Item Number", 12),
+                                   wcpadding("Item Name", 12), wcpadding("Item Cost", 12)))
         for item in item_list:
-            print("{0}|{1}|{2}".format(wcpadding(str(item[0]), 12), wcpadding(str(item[1]), 12), wcpadding(str(item[2]), 12)))
+            print("{0}|{1}|{2}".format(wcpadding(str(item[0]), 12), wcpadding(
+                str(item[1]), 12), wcpadding(str(item[2]), 12)))
         return True
 
-def make_purchase():
+# Employee Summary
+
+
+def print_employees():
+    # print employees
+    if len(employee_list) == 0:
+        print("There is no Employee..")
+    else:
+        print("{0}|{1}|{2}|{3}|{4}|{5}|{6}".format(wcpadding("Employee ID", 12), wcpadding("Employee Name", 15), wcpadding("Employee Type", 15), wcpadding(
+            "Years Worked", 15), wcpadding("Total Purchased", 17), wcpadding("Total Discount", 17), wcpadding("Employee Discount Number", 15)))
+        for employee in employee_list:
+            print("{0}|{1}|{2}|{3}|{4}|{5}|{6}".format(wcpadding(str(employee[0]), 12), wcpadding(str(employee[1]), 15), wcpadding(str(employee[2]), 15), wcpadding(
+                str(employee[3]), 15), wcpadding(str(employee[4]), 17), wcpadding(str(employee[5]), 17), wcpadding(str(employee[6]), 15)))
+
+
+def calculate_discount():
+    print("Calculating..")
+    return
+
+
+def check_employee_and_item():
     employee_exist = False
     while not employee_exist:
-        discount_num = int_input("Please enter the Employee's discount number: ")
+        discount_num, _ = int_input(
+            "Please enter the Employee's discount number: ")
         for employee in employee_list:
             if employee[6] == discount_num:
                 employee_exist = True
                 break
-        print(f"There is no employee who has {discount_num} as a discount number..")
+        if not employee_exist:
+            print(
+                f"There is no employee who has {discount_num} as a discount number..")
 
     item_exist = False
-    while not employee:
-        item_num = int_input("Please enter an Item's ID: ")
+    while not item_exist:
+        item_num, _ = int_input("Please enter an Item's ID: ")
+        for item in item_list:
+            if item[0] == item_num:
+                item_exist = True
+                break
+        if not item_exist:
+            print(f"There is no item {item_num} to purchase..")
+
+    calculate_discount()
+
 
 def make_purchase_page():
     if not print_items():
-        print("You cannot make a purchase, since there is no item..")
+        print("You cannot make a purchase..")
         return
+    elif len(employee_list) == 0:
+        print("Since there is no employee, you cannot make a purchase")
     else:
-        make_purchase()
-
-
-# Employee Summary
-def print_employees():
-    if len(employee_list) == 0:
-        print("There is no Employee..")
-    else:
-        print("{0}|{1}|{2}|{3}|{4}|{5}|{6}".format(wcpadding("Employee ID", 12), wcpadding("Employee Name", 15), wcpadding("Employee Type", 15), wcpadding("Years Worked", 15), wcpadding("Total Purchased", 17), wcpadding("Total Discount", 17), wcpadding("Employee Discount Number", 15)))
-        for employee in employee_list:
-            print("{0}|{1}|{2}|{3}|{4}|{5}|{6}".format(wcpadding(str(employee[0]), 12), wcpadding(str(employee[1]), 15), wcpadding(str(employee[2]), 15), wcpadding(str(employee[3]), 15), wcpadding(str(employee[4]), 17), wcpadding(str(employee[5]), 17), wcpadding(str(employee[6]), 15)))
         while True:
-            print()
+            check_employee_and_item()
+            yes_or_no = input("\nAnother purchase? [y/n] ")
+            if yes_or_no == 'n':
+                break
+            elif yes_or_no == 'y':
+                continue
+            else:
+                print("Enter between 'y' or 'n'..")
+
+        print_employees()
+
+        while True:
             yes_or_no = input("Go to main menu? [y/n] ")
             if yes_or_no == 'y':
                 return
@@ -177,7 +234,6 @@ def print_employees():
                 sys.exit("Exiting Application..")
             else:
                 print("Enter between 'y' or 'n'..")
-
 
 
 def print_menu():
@@ -188,6 +244,7 @@ def print_menu():
     print("| 4-All Employee Summary |")
     print("| 5-Exit                 |")
     print("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n")
+
 
 def run():
     while True:
@@ -203,5 +260,6 @@ def run():
             print_employees()
         elif choice == 5:
             sys.exit("Exiting Application..")
+
 
 run()
